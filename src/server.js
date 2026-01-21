cfe048158b57dcdbbce1347975250fb4622ffc96
abhinnvyas <abhinnvyas2@gmail.com>
2026-01-21 16:31:01 +0530

    cors removed

--- src/server.js
+++ src/server.js
@@ -25,13 +25,12 @@ import authRoutes from "./routers/authRoutes.js";
 
 const app = express();
 
-// Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   }),
+// );
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
